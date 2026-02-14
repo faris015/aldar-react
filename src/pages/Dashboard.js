a7a71399e54165e02f152edcd63f1ec30a52d722
sulmi <sulmifari@gmail.com>
2026-02-14 10:50:20 +0530

    dummy commit

--- src/pages/Dashboard.js
+++ src/pages/Dashboard.js
@@ -2,7 +2,7 @@ function Dashboard() {
   return (
     <section>
       <h1>Dashboard</h1>
-      <p>This is a sample dashboard page using react-router-dom routing.</p>
+      <p>This is sample dashboard page using react-router-dom routing.</p>
     </section>
   );
 }
